6b0a5f97cf7193be898a603151a347c60161cf5c
ktouy <ktofcl@gmail.com>
2020-07-17 18:50:19 -0300

    clean in code

--- js/jqueryExample2.js
+++ js/jqueryExample2.js
@@ -1,87 +1,78 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    $(".property-image-title h5").css("width", "10px")
 
-  $(".property-image-title h5").css("width", "10px")
-  $("#card1").on('mouseover', function() {
-    // $(this).css("height", "20rem")
-    $(this).css("background-color", "rgb(67, 67, 78)")
-    $("#card1 h5:contains('KTO')").css("color", "#f3eeff")
-    $("p").css("color", "rgb(187, 255, 0)")
-    $("h1").css("color", "white")
-    $('#to*').css("color", "white")
-  })
-  $("#card1").on('mouseleave', function() {
-    // $(this).css("height", "18rem")
-    $(this).css("background-color", "#ededed")
-    $("h5:contains('KTO')").css("color", "#262535")
-    $("p").css("color", "")
-    $("h1").css("color", "")
-    $('#to*').css("color", "")
-  })
+    //Mouse OVer
+    $("#card1").on('mouseover', function () {
+        $(this).css("background-color", "rgb(67, 67, 78)")
+        $("#card1 h5:contains('KTO')").css("color", "#f3eeff")
+        $("p").css("color", "rgb(187, 255, 0)")
+        $("input#to").css("border", "1px solid rgb(187, 255, 0)")
+        $("h1").css("color", "white")
+        $('#to*').css("color", "white")
+    })
 
-  $("#card1").on('click tap touchstart', function() {
+    //Mouse Leave
+    $("#card1").on('mouseleave', function () {
+        $(this).css("background-color", "#ededed")
+        $("h5:contains('KTO')").css("color", "#262535")
+        $("input#to").css("border", "1px solid black")
+        $("p").css("color", "")
+        $("h1").css("color", "")
+        $('#to*').css("color", "")
+    })
 
+    $('span#timesHMS').on("mouseover", function(){
+        $(this).css("background", "#262535")
+        $(this).addClass("card blue-grey darken-1")
+    })
+    $('span#timesHMS').on("mouseleave", function () {
+        $(this).css("background", "")
+        $(this).removeClass("card blue-grey darken-1")
+    })
 
-    if (($(this).css('width') > '420px')) {
-      // $(".property-description").toggleClass("hide")
-      // $(this).css("width", "28rem")
-      // $(this).css("height", "18rem")
-      // $('#clock').toggleClass("hide")
-      // $("body").css("box-sizing", "inherit")
-      // $("p").css("color", "")
-      // $("h1").css("color", "")
+    //Click
+    $("#card1").on('click', function () {
+        if (($(this).css('width') > '420px')) {
+            // $(".property-description").toggleClass("hide")
+            // $(this).css("width", "28rem")
+            // $(this).css("height", "18rem")
+            // $('#clock').toggleClass("hide")
+            // $("body").css("box-sizing", "inherit")
+            // $("p").css("color", "")
+            // $("h1").css("color", "")
+        } else {
+            $('span.thumb').addClass("hide")
+            $(".property-description").toggleClass("hide")
+            $("body").css("box-sizing", "content-box")
+            $(".property-card > a").css("width", "inherit")
+            $(".property-image").css("width", "auto")
+            $(".property-description").toggleClass("hide")
+            $(".property-image-title h5").css("width", "10px")
+            $(this).css("width", "800px" )
+            $(this).css("height", "950px")
+            $('#clock').toggleClass("hide")
+            $("p").css("color", "white")
+            $("h1").css("color", "white")
+        }
+    })
 
-    } else {
-      $(".property-description").toggleClass("hide")
-      $("body").css("box-sizing", "content-box")
-      $(".property-card > a").css("width", "inherit")
-      $(".property-description").toggleClass("hide")
-      $(".property-image-title h5").css("width", "10px")
-      $(this).css("width", "800px")
-      $(this).css("height", "950px")
-      $('#clock').toggleClass("hide")
-      $("p").css("color", "white")
-      $("h1").css("color", "white")
-    }
+    // $("[data-clickable='true']").on('click', function () {
+    //     console.log('wazaaa')
+    // });
 
-
-  })
-
-
-
-
-
-
-
-
-
-
-  $('.property-social-icons').on('click tap touchstart', function() {
-    if (($("#card1").css('width') != '420px')) {
-      $(".property-description").toggleClass("hide")
-      $("#card1").css("width", "28rem")
-      $("#card1").css("height", "18rem")
-      $('#clock').toggleClass("hide")
-      $("body").css("box-sizing", "inherit")
-      $("p").css("color", "")
-      $("h1").css("color", "")
-
-    }
-
-  })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //Click box
+    $('.property-social-icons').on('click', function () {
+        if (($("#card1").css('width') != '420px')) {
+            $(".property-image").css("width", "28rem")
+            $(".property-description").toggleClass("hide")
+            $("#card1").css("width", "28rem")
+            $("#card1").css("height", "18rem")
+            $('#clock').toggleClass("hide")
+            $("body").css("box-sizing", "inherit")
+            $("p").css("color", "")
+            $("h1").css("color", "")
+        }
+    })
 
 
 });
