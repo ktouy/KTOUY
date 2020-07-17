383d1115c1658437ec1a130b0f9bb63f06a912bf
ktouy <ktofcl@gmail.com>
2020-07-17 01:24:03 -0300

    corrections

--- js/jqueryExample2.js
+++ js/jqueryExample2.js
@@ -1,61 +1,50 @@
-$(document).ready(function () {
-    $(".property-image-title h5").css("width", "10px")
-    $("#card1").on('mouseover', function () {
-        // $(this).css("height", "20rem")
-        $(this).css("background-color", "rgb(67, 67, 78)")
-        $("#card1 h5:contains('KTO')").css("color", "#f3eeff")
-        $("p").css("color", "rgb(187, 255, 0)")
-        $("h1").css("color", "white")
-        $('#to*').css("color", "white")
-    })
-    $("#card1").on('mouseleave', function () {
-        // $(this).css("height", "18rem")
-        $(this).css("background-color", "#ededed")
-        $("h5:contains('KTO')").css("color", "#262535")
-        $("p").css("color", "")
-        $("h1").css("color", "")
-        $('#to*').css("color", "")
-    })
+$(document).ready(function() {
 
-    $("#card1").on('click', function () {
+  $(".property-image-title h5").css("width", "10px")
+  $("#card1").on('mouseover', function() {
+    // $(this).css("height", "20rem")
+    $(this).css("background-color", "rgb(67, 67, 78)")
+    $("#card1 h5:contains('KTO')").css("color", "#f3eeff")
+    $("p").css("color", "rgb(187, 255, 0)")
+    $("h1").css("color", "white")
+    $('#to*').css("color", "white")
+  })
+  $("#card1").on('mouseleave', function() {
+    // $(this).css("height", "18rem")
+    $(this).css("background-color", "#ededed")
+    $("h5:contains('KTO')").css("color", "#262535")
+    $("p").css("color", "")
+    $("h1").css("color", "")
+    $('#to*').css("color", "")
+  })
 
-
-        if (($(this).css('width') != '420px')) {
-            // $(".property-description").toggleClass("hide")
-            // $(this).css("width", "28rem")
-            // $(this).css("height", "18rem")
-            // $('#clock').toggleClass("hide")
-            // $("body").css("box-sizing", "inherit")
-            // $("p").css("color", "")
-            // $("h1").css("color", "")
-
-        } else {
+  $("#card1").on('click tap touchstart', function() {
 
 
+    if (($(this).css('width') > '420px')) {
+      // $(".property-description").toggleClass("hide")
+      // $(this).css("width", "28rem")
+      // $(this).css("height", "18rem")
+      // $('#clock').toggleClass("hide")
+      // $("body").css("box-sizing", "inherit")
+      // $("p").css("color", "")
+      // $("h1").css("color", "")
 
-            // $('.property-image').toggleClass("hide")
-            $(".property-description").toggleClass("hide")
-            $("body").css("box-sizing", "content-box")
-            $(".property-card > a").css("width", "inherit")
-            // $(".property-image").css("width", "inherit")
-            $(".property-description").toggleClass("hide")
-            $(".property-image-title h5").css("width", "10px")
-            $(this).css("width", "800px")
-            $(this).css("height", "950px")
-            // $(this).css("margin-top", "200px")
-            // $(this).css("margin-bottom", "200px")
-            // $(".center").css("margin-top", "10px")
-            $('#clock').toggleClass("hide")
-            $("p").css("color", "white")
-            $("h1").css("color", "white")
-        }
+    } else {
+      $(".property-description").toggleClass("hide")
+      $("body").css("box-sizing", "content-box")
+      $(".property-card > a").css("width", "inherit")
+      $(".property-description").toggleClass("hide")
+      $(".property-image-title h5").css("width", "10px")
+      $(this).css("width", "800px")
+      $(this).css("height", "950px")
+      $('#clock').toggleClass("hide")
+      $("p").css("color", "white")
+      $("h1").css("color", "white")
+    }
 
 
-    })
-
-    $("[data-clickable='true']").on('click', function () {
-        this.preventDefault();
-    });
+  })
 
 
 
@@ -64,23 +53,21 @@ $(document).ready(function () {
 
 
 
-    $('.property-social-icons').on('click', function () {
-        if (($("#card1").css('width') != '420px')) {
-            // $('.property-image').toggleClass("hide")
-            $(".property-description").toggleClass("hide")
-            // $(".property-description").css("width", "28rem")
-            $("#card1").css("width", "28rem")
-            $("#card1").css("height", "18rem")
-            // $(this).css("margin", "15px")
-            // $(".center").css("margin-left", "50px")
-            $('#clock').toggleClass("hide")
-            $("body").css("box-sizing", "inherit")
-            $("p").css("color", "")
-            $("h1").css("color", "")
 
-        }
 
-    })
+  $('.property-social-icons').on('click tap touchstart', function() {
+    if (($("#card1").css('width') != '420px')) {
+      $(".property-description").toggleClass("hide")
+      $("#card1").css("width", "28rem")
+      $("#card1").css("height", "18rem")
+      $('#clock').toggleClass("hide")
+      $("body").css("box-sizing", "inherit")
+      $("p").css("color", "")
+      $("h1").css("color", "")
+
+    }
+
+  })
 
 
 
