ca81c312abb088801e945a3b5a7d41f9f30c4a9b
ktouy <ktofcl@gmail.com>
2020-07-17 21:51:02 -0300

    test

--- js/jqueryExample2.js
+++ js/jqueryExample2.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
     $(".property-image-title h5").css("width", "10px")
 
+
+
+
+
+
     //Mouse OVer
     $("#card1").on('mouseover', function () {
-        $(this).css("background-color", "rgb(67, 67, 78)")
+        $(this).css("background-color", "#262731")
         $("#card1 h5:contains('KTO')").css("color", "#f3eeff")
         $("p").css("color", "rgb(187, 255, 0)")
         $("input#to").css("border", "1px solid rgb(187, 255, 0)")
@@ -23,16 +28,18 @@ $(document).ready(function () {
 
     $('span#timesHMS').on("mouseover", function(){
         $(this).css("background", "#262535")
-        $(this).addClass("card blue-grey darken-1")
+        $(this).addClass("card  grey darken-4")
     })
     $('span#timesHMS').on("mouseleave", function () {
         $(this).css("background", "")
-        $(this).removeClass("card blue-grey darken-1")
+        $(this).removeClass("card  grey darken-4")
     })
 
     //Click
     $("#card1").on('click', function () {
-        if (($(this).css('width') > '420px')) {
+        if (!$(this).has('cardUp')) {
+            $(this).toggleClass('cardUp')
+            // ($(this).css('width') > '420px')
             // $(".property-description").toggleClass("hide")
             // $(this).css("width", "28rem")
             // $(this).css("height", "18rem")
@@ -48,8 +55,11 @@ $(document).ready(function () {
             $(".property-image").css("width", "auto")
             $(".property-description").toggleClass("hide")
             $(".property-image-title h5").css("width", "10px")
-            $(this).css("width", "800px" )
-            $(this).css("height", "950px")
+
+            $(this).toggleClass('cardUp')
+            // $(this).css("width", "70vh" )
+            // $(this).css("height", "80vh")
+
             $('#clock').toggleClass("hide")
             $("p").css("color", "white")
             $("h1").css("color", "white")
@@ -62,11 +72,14 @@ $(document).ready(function () {
 
     //Click box
     $('.property-social-icons').on('click', function () {
-        if (($("#card1").css('width') != '420px')) {
-            $(".property-image").css("width", "28rem")
+        if (!$("#clock").hasClass('hide')) {
+            $(".property-image").css("width", "")
             $(".property-description").toggleClass("hide")
-            $("#card1").css("width", "28rem")
-            $("#card1").css("height", "18rem")
+
+            $('#card1').toggleClass('cardUp')
+            // $("#card1").css("width", "")
+            // $("#card1").css("height", "")
+
             $('#clock').toggleClass("hide")
             $("body").css("box-sizing", "inherit")
             $("p").css("color", "")
